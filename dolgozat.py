a7bbd76f87f7d40349a28843ca640c8f7c871f4e
Levi-gal <90601416+Levi-gal@users.noreply.github.com>
2021-10-20 14:13:38 +0200

    Add files via upload

---
+++ dolgozat.py
@@ -0,0 +1,7 @@
+a = int(input('Első szám: '))
+b = int(input('Második szám: '))
+print('A két szám összege: ',  a + b )
+print ('A két szám különbsége: ', a - b )
+print('A két szám szorzata: ',  a * b )
+print('A két szám hányadosa: ',  a / b )
+
